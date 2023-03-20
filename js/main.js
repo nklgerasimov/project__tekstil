c728e85094c6f8432956b6e6d24aa60d6292aa42
nklgerasimov <gniko@yandex.ru>
2023-03-20 16:45:52 +0300

    final

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        // center: true,
+        mouseDrag: false,
+        touchDrag: false,
+        pullDrag: false,
+        // freeDrag: true,
+        loop: true,
+        margin: 0,
+        items: 4,
+        // nav: true,
+        // lazyLoad: true
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+    });
+});
